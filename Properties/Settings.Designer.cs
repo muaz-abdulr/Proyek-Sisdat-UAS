f87a16428ec99d6dac7ce3880bd70b723de37492
muaz <muaz20001@mail.unpad.ac.id>
2021-05-11 14:57:58 +0700

    menghapus connectionstring

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -22,16 +22,5 @@ namespace Sisdat_Movie_List.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Entertaintmen" +
-            "t.mdf;Integrated Security=True")]
-        public string MovieList {
-            get {
-                return ((string)(this["MovieList"]));
-            }
-        }
     }
 }
