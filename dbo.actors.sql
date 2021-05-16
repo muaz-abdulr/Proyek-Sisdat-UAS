83439f850dc0983974b4e9b53868687e64ce3f50
daffa alifiandi <daffalif02@gmail.com>
2021-05-16 20:35:30 +0700

    ganti gitignore mdf dan ldf

---
+++ dbo.actors.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[actors] (
+    [actor_id]   INT          NOT NULL,
+    [actor_name] VARCHAR (40) NULL,
+    [gender]     VARCHAR (10) NULL,
+    [age]        INT          NULL,
+    [bio]        TEXT         NULL,
+    [apala] NCHAR(10) NULL, 
+    PRIMARY KEY CLUSTERED ([actor_id] ASC)
+);
+
